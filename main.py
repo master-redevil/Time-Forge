9b7196ad95e562507cce7ce2d5a0414798abec33
M.Arif Manzoor <muhammadarifmanzoor15@gmail.com>
2026-05-09 01:09:14 +0500

    replaced Keyboard Library with windows official 'Register Hotkey' Library

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import sys
-import keyboard
+import ctypes
+from ctypes import wintypes
 from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 from PySide6.QtGui import QIcon, QPixmap, QColor, QPainter
-from PySide6.QtCore import QObject, Signal, Slot, Qt
+from PySide6.QtCore import QObject, Signal, Slot, Qt, QAbstractNativeEventFilter
 
 import database
 from tracker import TrackerDaemon
 from ui.dashboard import DashboardWindow
+
+class NativeHotkeyFilter(QAbstractNativeEventFilter):
+    def __init__(self, hotkey_id, signal):
+        super().__init__()
+        self.hotkey_id = hotkey_id
+        self.signal = signal
+
+    def nativeEventFilter(self, eventType, message):
+        # P1.4: Listen for WM_HOTKEY message (0x0312)
+        if eventType == "windows_generic_MSG":
+            msg = wintypes.MSG.from_address(message.__int__())
+            if msg.message == 0x0312: # WM_HOTKEY
+                if msg.wParam == self.hotkey_id:
+                    self.signal.emit()
+                    return True, 0
+        return False, 0
 
 class AppController(QObject):
     show_dashboard_sig = Signal()
@@ -30,7 +47,18 @@ class AppController(QObject):
         
         # Connect signal for thread-safe UI update from hotkey
         self.show_dashboard_sig.connect(self.toggle_dashboard)
-        keyboard.add_hotkey('ctrl+shift+t', self.show_dashboard_sig.emit)
+        
+        # P1.4: Native Hotkey Registration (Ctrl+Shift+T)
+        self.HOTKEY_ID = 1
+        self.hotkey_filter = NativeHotkeyFilter(self.HOTKEY_ID, self.show_dashboard_sig)
+        QApplication.instance().installNativeEventFilter(self.hotkey_filter)
+        
+        MOD_CONTROL = 0x0002
+        MOD_SHIFT = 0x0004
+        VK_T = 0x54 # 'T' key
+        
+        if not ctypes.windll.user32.RegisterHotKey(None, self.HOTKEY_ID, MOD_CONTROL | MOD_SHIFT, VK_T):
+            print("Warning: Could not register global hotkey Ctrl+Shift+T")
 
     def setup_tray(self):
         import os
@@ -79,6 +107,9 @@ class AppController(QObject):
             self.dashboard_window.activateWindow()
 
     def quit_app(self):
+        # P1.4: Properly unregister hotkey to release system resource
+        if hasattr(self, 'HOTKEY_ID'):
+            ctypes.windll.user32.UnregisterHotKey(None, self.HOTKEY_ID)
         self.tracker.stop()
         QApplication.quit()
 
